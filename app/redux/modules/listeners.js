f21fa8d52bd30b6f0479794e3ebe63b97c14e8c3
Dvir Rezenman <dvir.rez@gmail.com>
2018-01-17 15:49:37 +0200

    listener redux module

---
+++ app/redux/modules/listeners.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Dvir on 1/17/2018.
+ */
+const ADD_LISTENER = 'ADD_LISTENER';
+
+function addListener(listenerId) {
+    return {
+        type: ADD_LISTENER,
+        listenerId,
+    };
+}
+
+export default function listeners (state = {}, action) {
+    switch (action.type) {
+        case ADD_LISTENER :
+            return {
+                ...state,
+                [action.listenerId]: true,
+            };
+        default :
+            return state;
+    }
+}
