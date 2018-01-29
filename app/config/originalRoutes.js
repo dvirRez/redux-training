133724bbcbe85d3cae4aae194f15452c3f4f0286
Dvir Rezenman <dvir.rez@gmail.com>
2018-01-29 17:00:26 +0200

    Backup original files

---
+++ app/config/originalRoutes.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Dvir on 1/4/2018.
+ */
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MainContainer, HomeContainer,
+		 AuthenticateContainer, FeedContainer,
+		 LogoutContainer, UserContainer, DuckDetailsContainer } from 'containers';
+
+export default function getRoutes(checkAuth) {
+	return (
+		<BrowserRouter>
+			<MainContainer>
+				<Switch>
+					<Route exact={true} path='/' component={checkAuth(HomeContainer)} />
+					<Route path='/login' component={checkAuth(AuthenticateContainer)} />
+					<Route path='/feed' component={checkAuth(FeedContainer)} />
+					<Route path='/logout' component={LogoutContainer} />
+                    <Route path='/duckDetail/:duckId' component={checkAuth(DuckDetailsContainer)}/>
+					<Route path='/:uid' component={checkAuth(UserContainer)} />
+				</Switch>
+			</MainContainer>
+		</BrowserRouter>
+    );
+}
+
+
---
+++ app/originalIndex.js
@@ -0,0 +1,26 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import getRoutes from './config/routes';
+import restricted from 'helpers/restricted';
+import thunk from 'redux-thunk';
+import * as reducers from 'redux/modules';
+
+const store = createStore(combineReducers(reducers), compose(
+    applyMiddleware(thunk),
+	window.devToolsExtension ? window.devToolsExtension() : (f) => f
+));
+
+console.log(store.getState());
+
+function checkAuth(component) {
+	return restricted(component, store);
+}
+
+ReactDom.render(
+	<Provider store={store}>
+		{getRoutes(checkAuth)}
+	</Provider>,
+	document.getElementById('app')
+);
